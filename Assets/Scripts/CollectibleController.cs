48435ec1ab3485fd4439066c5d26a1f81f57a37a
Josch21 <139594929+Josch21@users.noreply.github.com>
2023-11-07 18:51:03 -0500

    Collectible

---
+++ Assets/Scripts/CollectibleController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectibleController : MonoBehaviour
+{
+    public int collectible = 3;
+
+    public Image[] crowns;
+    public Sprite fullCrown;
+    public Sprite emptyCrown;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
